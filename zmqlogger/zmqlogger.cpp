67d14552761a9c900f97a943a7e25dd0a110f79a
d-led <dmlled@yahoo.com>
2013-02-24 17:59:19 +0100

    g2log

--- zmqlogger/zmqlogger.cpp
+++ zmqlogger/zmqlogger.cpp
@@ -1,26 +1,30 @@
 #include "stdafx.h"
 
+using optparse::OptionParser;
+
+static void configure_parser(OptionParser parser)
+{
+	parser.description("0mq json logger");
+
+	parser.add_option("-p", "--port")
+				.dest("port")
+				.set_default("5555")
+				.help("set listening port");
+}
+
 int main(int argc, char* argv[])
 {
-	using optparse::OptionParser;
-
     // prepare context and socket
     zmq::context_t context (1);
     zmq::socket_t socket (context, ZMQ_REP);
 
 	// prepare options
-	OptionParser parser = OptionParser().description("0mq json logger");
-
-	parser.add_option("-p", "--port")
-                  .dest("port")
-				  .set_default("5555")
-                  .help("set listening port");
-
+	OptionParser parser = OptionParser();
+	configure_parser(parser);
 	optparse::Values options = parser.parse_args(argc, argv);
 
 	std::string port=options.get("port");
 	std::string socket_config="tcp://*:";
-
 	socket_config+=port;
 
 	socket.bind (socket_config.c_str());
@@ -33,9 +37,6 @@ int main(int argc, char* argv[])
         //  Wait for next request from client
         socket.recv (&request);
         std::cout << "Received Hello" << std::endl;
-
-        //  Do some 'work'
-        //sleep (1);
 
         //  Send reply back to client
         zmq::message_t reply (5);
