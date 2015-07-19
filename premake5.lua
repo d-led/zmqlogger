6bcbe1a019a4053649c7ade104f7c02aba50fddc
Dmitry Ledentsov <dmlled@yahoo.com>
2015-07-19 22:24:48 +0200

    fixed linker error, compiling g3log cpps

--- premake5.lua
+++ premake5.lua
@@ -2,15 +2,35 @@ include 'premake'
 
 make_solution 'zmqlogger'
 
+g3log_root = 'deps/g3log-7cf42c535c3be83eec1ff0c374a626a2b45a033b/src'
+
 includedirs {
-	'deps/g3log-7cf42c535c3be83eec1ff0c374a626a2b45a033b/src'
+	g3log_root
 }
 
 defines {
 	'CHANGE_G3LOG_DEBUG_TO_DBUG',
 }
 
+make_static_lib('g3log',{
+	g3log_root..'/**.cpp'
+})
+
+	configuration 'windows'
+		excludes {
+			g3log_root..'/g3log/crashhandler_unix.cpp'
+		}
+	configuration '*'
+
 make_console_app('g2log_benchmark',{
 	'g2log_benchmark/*.h',
 	'g2log_benchmark/*.cpp'
 })
+
+	configuration 'windows'
+		links 'dbghelp'
+	configuration '*'
+
+	links {
+		'g3log'
+	}
